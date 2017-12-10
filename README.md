f6152d1d82cc59813670e047f3cceef711452942
Tudor-Dan Ravoiu <tudor.ravoiu@plentymarkets.com>
2017-12-10 20:05:00 +0100

    -

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# radioslack
+# Chuck Norris Slack Bot
+
+Just run `BOT_API_KEY=slack_api-key node bin/bot.js`and Chuck Norris will hit you.
---
+++ src/norrisbot.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var util = require('util');
+var path = require('path');
+var fs = require('fs');
+var SQLite = require('sqlite3').verbose();
+var Bot = require('slackbots');
+
+var NorrisBot = function Constructor(settings) {
+    this.settings = settings;
+    this.settings.name = settings.name || 'norrisbot';
+    this.dbPath = settings.dbPath || path.resolve(process.cwd(), 'data', 'norrisbot.db');
+
+    this.user = null;
+    this.db = null
+};
+
+util.inherits(NorrisBot, Bot);
+
+module.exports = NorrisBot;
+
+NorrisBot.prototype.run = function () {
+    NorrisBot.super_.call(this, this.settings);
+
+    this.on('start', this._onStart);
+    this.on('message', this._onMessage);
+};
+
+NorrisBot.prototype._onStart = function () {
+    this._loadBotUser();
+    this._connectDb();
+    this._firstRunCheck();
+};
+
+NorrisBot.prototype._loadBotUser = function () {
+    var self = this;
+
+    this.user = this.users.filter(function (user) {
+        return user.name === self.name;
+    })[0];
+};
+
+NorrisBot.prototype._connectDb = function () {
+    if (!fs.existsSync(this.dbPath)) {
+        console.error('Database path ' + '"' + this.dbPath + '" does not exists or it\'s not readable.');
+
+        process.exit();
+    }
+
+    this.db = new SQLite.Database(this.dbPath);
+};
+
+NorrisBot.prototype._firstRunCheck = function () {
+    var self = this;
+
+    self.db.get('SELECT val FROM info WHERE name = "lastrun" LIMIT 1', function (err, record) {
+        if (err) {
+            return console.error('DATABASE ERROR:', err);
+        }
+
+        var currentTime = (new Date()).toJSON();
+
+        if (!record) {
+            self._welcomeMessage();
+            return self.db.run('INSERT INTO info(name, val) VALUES("lastrun", ?)', currentTime);
+        }
+
+        self.db.run('UPDATE info SET val = ? WHERE name = "lastrun"', currentTime);
+    });
+};
+
+NorrisBot.prototype._welcomeMessage = function () {
+    this.postMessageToChannel(this.channels[0].name, 'Hi there! \n I\'m the Norris Bot. I tell you fun facts about ' +
+        'Chuck Norris if you say `Chuck Norris` or `' + this.name + '`. \n BtW, I was developed by the greates person ' +
+        'on earth, Tudor-Dan Ravoiu', {
+        as_user: true
+    })
+};
+
+NorrisBot.prototype._onMessage = function (message) {
+    if (this._isChatMessage(message) &&
+        this._isChannelConversation(message) &&
+        !this._isFromNorrisBot(message) &&
+        this._isMentioningChuckNorris(message)
+    ) {
+        this._replyWithRandomJoke(message);
+    }
+};
+
+NorrisBot.prototype._isChatMessage = function (message) {
+    return message.type === 'message' && Boolean(message.text);
+};
+
+NorrisBot.prototype._isChannelConversation = function (message) {
+    return typeof message.channel === 'string' && message.channel[0] === 'C';
+};
+
+NorrisBot.prototype._isFromNorrisBot = function (message) {
+    return message.user === this.user.id;
+};
+
+NorrisBot.prototype._isMentioningChuckNorris = function (message) {
+    return message.text.toLowerCase().indexOf('chuck norris') > -1 || message.text.toLowerCase().indexOf(this.name) > -1;
+};
+
+NorrisBot.prototype._replyWithRandomJoke = function (originalMessage) {
+    var self = this;
+
+    self.db.get('SELECT id, joke FROM jokes ORDER BY used ASC, RANDOM() LIMIT 1', function (err, record) {
+        if (err) {
+            return console.error('DATABASE ERROR:', err);
+        }
+
+        var channel = self._getChannelById(originalMessage.channel);
+        self.postMessageToChannel(channel.name, record.joke, {as_user: true});
+        self.db.run('UPDATE jokes SET used = used + 1 WHERE id = ?', record.id);
+    });
+};
+
+NorrisBot.prototype._getChannelById = function (channelId) {
+    return this.channels.filter(function (item) {
+        return item.id === channelId;
+    })[0];
+};
